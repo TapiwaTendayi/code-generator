6a9a9ae045b9d691854917a6a1b4572ab44828c2
TapiwaTendayi <tapiwatatenda123@gmail.com>
2025-02-06 08:08:25 +0200

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayCode(response) {
-  console.log("poem generated");
   new Typewriter("#code", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,15 +11,14 @@ function generateCode(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "60bfefo647de9cd06a8b9634a3cft9b4";
   let context =
-    "You are an intelligent programmer who is an expert at writing short code.Your mission is to generate a four line code in basic HTML,but do not include the basic HTML on the response and separate each line with a </br>.Do not include a title. Sign the code with 'SheCodes AI' inside a <strong> element at the end of the code .Make sure to follow the user instructons below";
+    "You are an intelligent programmer who is an expert at writing short code.Your mission is to generate a short code  and separate each line with a </br>.Do not include a title.If the user does not specify the language use python as the default langauge. Sign the code with 'SheCodes AI' inside a <strong> element at the end of the code .Make sure to follow the user instructons below";
   let prompt = `User instructions are:Generate a code about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("generating code");
-  console.log(`Promp:${prompt}`);
-  console.log(`Context:${context}`);
+  let codeElement = document.querySelector("#code");
+  codeElement.classList.remove("hidden");
+  codeElement.innerHTML = `<div class="generating"> 🧑‍💻Generating  ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayCode);
 }
 let codeForm = document.querySelector("#code-generator-form");
 codeForm.addEventListener("submit", generateCode);
-
